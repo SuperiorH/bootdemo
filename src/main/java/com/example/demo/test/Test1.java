61a18b285c70f3d3b5654285f223ab152551cd60
JoeH <jianjian.huang@g-town.com.cn>
2019-01-16 14:02:10 +0800

    修改(三)

--- src/main/java/com/example/demo/test/Test1.java
+++ src/main/java/com/example/demo/test/Test1.java
@@ -2,11 +2,13 @@ package com.example.demo.test;
 
 import java.io.*;
 
+/**
+ * transient 关键词测试
+ */
 public class Test1 implements Serializable {
 
     private transient String name;
     private int age;
-    static String address = "江苏省";
 
     public Test1(String name, int age) {
         this.name = name;
@@ -24,14 +26,12 @@ public class Test1 implements Serializable {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         String path = "D:" + File.separator + "1.txt";
         Test1 t = new Test1("Joe", 18);
-        System.out.println(t.address);
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
         oos.writeObject(t);
         System.out.println("=======这是分割线======");
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
         Test1 tt = (Test1) ois.readObject();
         System.out.println(tt);
-        System.out.println(tt.address);
     }
 
 }
