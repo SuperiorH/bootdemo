9267a476ca1df917b83eceb692b1fd56f2455cf1
JoeH <jianjian.huang@g-town.com.cn>
2019-01-22 14:49:11 +0800

    修改(六)

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.jpa.entity.UseBuyerCollect;
+import com.example.demo.jpa.repository.UseBuyerCollectRepository;
+import com.example.demo.mongo.dao.UserDao;
+import com.example.demo.mongo.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
+
+	@Autowired
+	UseBuyerCollectRepository repository;
+
 	@Test
-	public void contextLoads() {
-		System.out.println(123);
+	public void test() {
+		List<UseBuyerCollect> list  = repository.findByUserId("1");
+		System.out.println("*********");
+		list.forEach(System.out::println);
+		System.out.println("*********");
 	}
 
 }
